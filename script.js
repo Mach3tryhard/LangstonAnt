073b8fa16d1fb634c592f95c1d9d1ef015b49b51
Matei Sirghe <matei.sirghe@gmail.com>
2025-01-04 19:51:30 +0200

    caca

--- script.js
+++ script.js
@@ -1,91 +1,111 @@
-//javascript:(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='https://mrdoob.github.io/stats.js/build/stats.min.js';document.head.appendChild(script);})()
+const canvas = document.createElement("canvas");
+canvas.width = 1920;
+canvas.height = 1000;
+document.body.appendChild(canvas);
+
 var pixels = [];
+var colors = [
+  "red",
+  "white",
+  "green",
+  "yellow",
+  "purple",
+  "orange",
+  "pink",
+  "brown",
+  "blue",
+  "fuchsia",
+  "aqua",
+  "lime",
+  "maroon",
+  "navy",
+  "olive",
+  "silver",
+  "teal",
+];
+var extdir = "RL";
+const ctx = canvas.getContext("2d");
+function ColorPixel(pozx, pozy, cycle) {
+  let pixel = {};
+  pixel.x = pozx;
+  pixel.y = pozy;
+  pixel.cycle = cycle;
 
-function ColorPixel(pozx,pozy){
-    let pixel = {};
-    pixel.x = pozx;
-    pixel.y = pozy;
+  ctx.fillStyle = colors[cycle];
+  ctx.fillRect(pozx, pozy, 1, 1);
 
-    pixel.getpixel = document.createElement("div");
-    pixel.getpixel.style.width = 1 + 'px';
-    pixel.getpixel.style.height = 1 + 'px';
-    pixel.getpixel.style.borderRadius = '100%';
-    pixel.getpixel.style.position = 'absolute';
-    pixel.getpixel.style.background = 'black';
-
-    pixel.getpixel.style.left = pixel.x + 'px';
-    pixel.getpixel.style.top = pixel.y + 'px';
-
-    document.body.appendChild(pixel.getpixel);
-    return pixel;
+  return pixel;
 }
 
-function CreateAnt(pozx,pozy){
-    let ant = {};
-    ant.x = pozx;
-    ant.y = pozy;
+function CreateAnt(pozx, pozy) {
+  let ant = {};
+  ant.x = pozx;
+  ant.y = pozy;
 
-    ant.getpixel = document.createElement("div");
-    ant.getpixel.style.width = 1 + 'px';
-    ant.getpixel.style.height = 1 + 'px';
-    ant.getpixel.style.borderRadius = '100%';
-    ant.getpixel.style.position = 'absolute';
-    ant.getpixel.style.background = 'red';
-    ant.getpixel.style.zIndex = 1;
-
-    document.body.appendChild(ant.getpixel);
-    return ant;
+  return ant;
 }
 
-function UpdateMove(ant)
-{
-    ant.getpixel.style.left = ant.x+'px';
-    ant.getpixel.style.top = ant.y+'px';
+function RemovePixel(ind) {
+  pixels.splice(ind, 1);
 }
 
-function RemovePixel(ind){
-    document.body.removeChild(pixels[ind].getpixel);
-
-    pixels.splice(ind, 1);
-}
-
-var ant = CreateAnt(185,90);
+var ant = CreateAnt(185, 90);
 
 var dir = 3;
 var step = 0;
-function update(){
-    step+=1;
-    var isWhite=true;
-    var rmpind =-1;
-    for(var i=0;i<pixels.length;i++){
-        if(pixels[i].x == ant.x && pixels[i].y == ant.y){
-            isWhite=false;
-            rmpind=i;
-        }
-    }
-    console.log(step);
-    if (isWhite) {
-        pixels.push(ColorPixel(ant.x,ant.y));
-        dir = (dir + 1) % 4;
-    } else {
-        RemovePixel(rmpind);
-        dir = (dir + 3) % 4;
-    }
-    switch (dir) {
-        case 0:
-            ant.x++;
-            break;
-        case 1:
-            ant.y++;
-            break;
-        case 2:
-            ant.x--;
-            break;
-        case 3:
-            ant.y--;
-            break;
-    }
-    UpdateMove(ant);
-}
 
-setInterval(update,1);
+function update() {
+  step += 1;
+  var CurrentColor = -1;
+  var rmpind = -1;
+  for (var i = 0; i < pixels.length; i++) {
+    if (pixels[i].x == ant.x && pixels[i].y == ant.y) {
+      CurrentColor = pixels[i].cycle;
+      rmpind = i;
+    }
+  }
+  console.log(step);
+  if (CurrentColor == -1) {
+    pixels.push(ColorPixel(ant.x, ant.y, 1));
+    if (extdir[0] == "L") dir = (dir + 1) % 4;
+    if (extdir[0] == "R") dir = (dir + 3) % 4;
+  } else if (extdir[CurrentColor] == "L") {
+    pixels[rmpind].cycle += 1;
+    if (pixels[rmpind].cycle > extdir.length - 1) {
+      ctx.fillStyle = "black";
+      ctx.fillRect(pixels[rmpind].x, pixels[rmpind].y, 1, 1);
+      RemovePixel(rmpind);
+    } else {
+      ctx.fillStyle = colors[pixels[rmpind].cycle];
+      ctx.fillRect(pixels[rmpind].x, pixels[rmpind].y, 1, 1);
+    }
+    dir = (dir + 1) % 4;
+  } else if (extdir[CurrentColor] == "R") {
+    pixels[rmpind].cycle += 1;
+    if (pixels[rmpind].cycle > extdir.length - 1) {
+      ctx.fillStyle = "black";
+      ctx.fillRect(pixels[rmpind].x, pixels[rmpind].y, 1, 1);
+      RemovePixel(rmpind);
+    } else {
+      ctx.fillStyle = colors[pixels[rmpind].cycle];
+      ctx.fillRect(pixels[rmpind].x, pixels[rmpind].y, 1, 1);
+    }
+    dir = (dir + 3) % 4;
+  }
+  switch (dir) {
+    case 0:
+      ant.x++;
+      break;
+    case 1:
+      ant.y++;
+      break;
+    case 2:
+      ant.x--;
+      break;
+    case 3:
+      ant.y--;
+      break;
+  }
+  setTimeout(update, 0);
+}
+update();
